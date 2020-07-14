4f89296f45ea6dbbe15c0b6d9482df35d66bf0eb
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-07-14 19:47:39 +0530

    first commit

---
+++ FirstFramework/Common.swift
@@ -0,0 +1,15 @@
+//
+//  Common.swift
+//  FirstFramework
+//
+//  Created by Pradeep on 14/07/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+import Foundation
+
+struct Common {
+    
+    static let shared = Common()
+    var name = "Pradeep Sagar"
+}
